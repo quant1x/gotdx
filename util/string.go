a8541f9af3f2045f1936d5f5990a924bee474505
wangfeng <wangfengxy@sina.cn>
2023-01-12 12:49:45 +0800

    修正ioutil包

--- util/string.go
+++ util/string.go
@@ -4,17 +4,14 @@ import (
 	"bytes"
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
-	"io/ioutil"
+	"io"
 	"strings"
 )
 
 func Utf8ToGbk(text []byte) string {
 
 	r := bytes.NewReader(text)
-
 	decoder := transform.NewReader(r, simplifiedchinese.GBK.NewDecoder()) //GB18030
-
-	content, _ := ioutil.ReadAll(decoder)
-
+	content, _ := io.ReadAll(decoder)
 	return strings.ReplaceAll(string(content), string([]byte{0x00}), "")
 }
