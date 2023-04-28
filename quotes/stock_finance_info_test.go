f21094d31b5abbca12c94f2ecabf709b64288b61
wangfeng <wangfengxy@sina.cn>
2023-04-29 07:10:42 +0800

    增加财务数据测试代码

---
+++ quotes/stock_finance_info_test.go
@@ -0,0 +1,25 @@
+package quotes
+
+import (
+	"encoding/json"
+	"fmt"
+	"gitee.com/quant1x/gotdx/proto"
+	"github.com/mymmsc/gox/api"
+	"testing"
+)
+
+func TestNewFinanceInfoPackage(t *testing.T) {
+	stdApi, err := NewStdApi()
+	if err != nil {
+		panic(err)
+	}
+	defer stdApi.Close()
+	sq1, err := stdApi.GetFinanceInfo(proto.MarketShangHai, "605577", 1)
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+	fmt.Printf("%+v\n", sq1)
+	data, _ := json.Marshal(sq1)
+	text := api.Bytes2String(data)
+	fmt.Println(text)
+}
