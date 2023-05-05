115c7b537fd9abdbe0de0cdbc8c7ca9e0c0899ac
wangfeng <wangfengxy@sina.cn>
2023-05-05 17:55:39 +0800

    增加测试代码

---
+++ quotes/stock_security_bars_test.go
@@ -0,0 +1,26 @@
+package quotes
+
+import (
+	"encoding/json"
+	"fmt"
+	"gitee.com/quant1x/gotdx/proto"
+	"github.com/mymmsc/gox/api"
+	"testing"
+)
+
+func TestSecurityBarsPackage(t *testing.T) {
+	stdApi, err := NewStdApiWithServers([]Server{Server{Host: "123.125.108.14", Port: 7709, Name: "test"}})
+	if err != nil {
+		panic(err)
+	}
+	defer stdApi.Close()
+	//sq1, err := stdApi.GetSecurityQuotes([]uint8{proto.MarketIdShangHai, proto.MarketIdShangHai, proto.MarketIdShangHai, proto.MarketIdShenZhen}, []string{"600275", "600455", "600086", "300742"})
+	sq1, err := stdApi.GetKLine(proto.MarketIdShenZhen, "000966", proto.KLINE_TYPE_RI_K, 0, 1)
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+	fmt.Printf("%+v\n", sq1)
+	data, _ := json.Marshal(sq1)
+	text := api.Bytes2String(data)
+	fmt.Println(text)
+}
