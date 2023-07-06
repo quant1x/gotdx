92fa17ec7d2e5f206ad664cb5bd63292b5e5f8de
wangfeng <wangfengxy@sina.cn>
2023-07-06 08:23:02 +0800

    收敛日历文件的文件名函数

--- internal/cache/cache_filename.go
+++ internal/cache/cache_filename.go
@@ -2,6 +2,12 @@ package cache
 
 import "fmt"
 
+// CalendarFilename 交易日历文件路径
+func CalendarFilename() string {
+	filename := GetMetaPath() + "/calendar"
+	return filename
+}
+
 // BlockFilename 板块缓存路径
 func BlockFilename(ns ...string) string {
 	// 默认取板块列表
@@ -9,6 +15,6 @@ func BlockFilename(ns ...string) string {
 	if len(ns) > 0 {
 		name = ns[0]
 	}
-	filename := fmt.Sprintf("%s/%s.csv", GetBlockPath(), name)
+	filename := fmt.Sprintf("%s/%s.csv", GetMetaPath(), name)
 	return filename
 }
