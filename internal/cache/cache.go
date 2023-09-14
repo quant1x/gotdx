8b618c0a639f1c7c3050307e49b70bab1ee0b7a6
wangfeng <wangfengxy@sina.cn>
2023-09-14 17:23:29 +0800

    按照不同的操作系统拆分默认的缓存路径

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,36 +2,24 @@ package cache
 
 import (
 	"gitee.com/quant1x/gox/util/homedir"
-	"runtime"
-)
-
-const (
-	cacheRootPathOfWin  = "c:/.quant1x"
-	cacheRootPathOfUinx = "~/.quant1x"
 )
 
 var (
-	__default_cache_path = "~/.quant1x" // 数据根路径
+	__global_cache_path = cache_default_path // 数据根路径
 )
 
 func init() {
 	// 初始化缓存路径
-	switch runtime.GOOS {
-	case "windows":
-		__default_cache_path = cacheRootPathOfWin
-	default:
-		__default_cache_path = cacheRootPathOfUinx
-	}
-	rootPath, err := homedir.Expand(__default_cache_path)
+	rootPath, err := homedir.Expand(__global_cache_path)
 	if err != nil {
 		panic(err)
 	}
-	__default_cache_path = rootPath
+	__global_cache_path = rootPath
 }
 
 // DefaultCachePath 数据缓存的根路径
 func DefaultCachePath() string {
-	return __default_cache_path
+	return __global_cache_path
 }
 
 // GetMetaPath 元数据缓存路径
---
+++ internal/cache/cache_windows.go
@@ -0,0 +1,7 @@
+//go:build windows
+
+package cache
+
+const (
+	cache_default_path = "c:/.quant1x"
+)
