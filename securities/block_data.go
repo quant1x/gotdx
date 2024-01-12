9fa7a7380531d8c498f7fbb05cb71a94b8a3d501
wangfeng <wangfengxy@sina.cn>
2024-01-12 14:39:10 +0800

    删除废弃的代码

--- securities/block_data.go
+++ securities/block_data.go
@@ -17,38 +17,6 @@ func syncBlockFiles() {
 	updateCacheBlockFile()
 }
 
-// 更新本地板块文件
-func v1UpdateBlockFile() {
-	// 如果板块数据不存在, 从应用内导出
-	blockFile := cache.BlockFilename()
-	createOrUpdate := false
-	if !api.FileExist(blockFile) {
-		createOrUpdate = true
-	}
-	if !createOrUpdate {
-		blockData := cache.GetBlockPath() + "/" + quotes.BLOCK_DEFAULT
-		dataStat, err := os.Stat(blockData)
-		if err == nil || os.IsExist(err) {
-			dataModifyTime := dataStat.ModTime()
-			// 检查通达信热股880818板块文件
-			bk880818 := cache.GetBlockPath() + "/" + "880818.csv"
-			bkStat, err := os.Stat(bk880818)
-			if err == nil || os.IsExist(err) {
-				if bkStat.ModTime().Before(dataModifyTime) {
-					createOrUpdate = true
-				}
-			} else {
-				createOrUpdate = true
-			}
-		} else {
-			createOrUpdate = true
-		}
-	}
-	if createOrUpdate {
-		parseAndGenerateBlockFile()
-	}
-}
-
 // 更新缓存csv数据文件
 func updateCacheBlockFile() {
 	// 如果板块数据不存在, 从应用内导出
