c237649ea87831e9a55e575501a60a0aa4cd6d22
wangfeng <wangfengxy@sina.cn>
2024-01-19 07:30:37 +0800

    删除废弃的同步脚本

--- push-gitee.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-git remote set-url origin https://gitee.com/quant1x/gotdx.git
-git checkout master
-git push
-git push --tags
-git remote -vv
--- push-github.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-git remote set-url origin https://github.com/quant1x/gotdx.git
-git checkout master
-git push
-git push --tags
-git remote -vv
