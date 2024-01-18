4485b96c0de00f138f0edd06f66bc617c054e839
wangfeng <wangfengxy@sina.cn>
2024-01-19 07:32:22 +0800

    备注基本的交易单位函数

--- internal/unit.go
+++ internal/unit.go
@@ -1,6 +1,11 @@
 package internal
 
 // BaseUnit 交易单位
+//
+//	A股、债券交易和债券买断式回购交易的申报价格最小变动单位为0.01元人民币
+//	基金、权证交易为0.001元人民币
+//	B股交易为0.001美元
+//	债券质押式回购交易为0.005元
 func BaseUnit(code string) float64 {
 	c := code[:2]
 	switch c {
