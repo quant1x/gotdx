2455784b4e00a1a730f5a8d2654aca14cc4b97c1
wangfeng <wangfengxy@sina.cn>
2024-05-19 17:47:43 +0800

    调整测试代码, 新增获取日K线的测试

--- tdx-client_test.go
+++ tdx-client_test.go
@@ -2,6 +2,7 @@ package gotdx
 
 import (
 	"fmt"
+	"gitee.com/quant1x/gotdx/proto"
 	"testing"
 	"time"
 )
@@ -15,4 +16,6 @@ func TestReOpen(t *testing.T) {
 	v, _ = api.GetXdxrInfo("sh600072")
 	fmt.Println(v)
 	fmt.Println(api.NumOfServers())
+	klines, _ := api.GetKLine("sh600600", proto.KLINE_TYPE_RI_K, 0, 1)
+	fmt.Println(klines)
 }
