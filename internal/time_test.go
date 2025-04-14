44f37c60fdc22706edcbc8159a521aec372bb34a
wangfeng <wangfengxy@sina.cn>
2025-04-14 09:01:24 +0800

    150006364 会解析成150:00:38.184, 看情况150需要除以10, 是整体除以10还是就小时除以10

--- internal/time_test.go
+++ internal/time_test.go
@@ -13,6 +13,7 @@ func TestTimeFromInt(t *testing.T) {
 	nTime = 11295421
 	nTime = 10100682
 	nTime = 8836243
+	nTime = 150006364
 	t1 := time.UnixMilli(int64(nTime))
 	fmt.Println(t1)
 	//nTime = 8
